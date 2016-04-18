47d164f091cedabadbabc283c91d06076c3f8084
Vitiok <lucavictor220@gmail.com>
2016-04-18 09:26:24 +0300

    Added application pages

---
+++ js/button-events.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('#web-task').click(function() {
+     window.open("applications/web.html");
+  });
+  $('#mobile-task').click(function() {
+     window.open("applications/mobile.html");
+  })
+  $('#2d-task').click(function() {
+     window.open("applications/2_D.html");
+  })
+  $('#3d-task').click(function() {
+     window.open("applications/3_D.html");
+  })
+});
